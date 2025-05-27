c2a932a863a9c6f0ed7749600e898535efd2f3d6
mndrake95 <mandra4a@gmail.com>
2025-05-27 16:07:26 +0500

    modified:   education/class_object_to_do_edu.py 	modified:   to_do/main.py

--- education/class_object_to_do_edu.py
+++ education/class_object_to_do_edu.py
@@ -1,20 +1,36 @@
+
 class Task:
     def __init__ (self, task_id, title, description, status):
-        self.task_id = task_id
+        self._task_id = task_id
         self.title = title
         self.description = description
-        self.status = status
+        self._status = status
     def display_info(self):
-        print(f"Task ID: {self.task_id}, Title: {self.title}, Description: {self.description}, Status: {self.status}")
+        print(f"Task ID: {self._task_id}, Title: {self.title}, Description: {self.description}, Status: {self._status}")
+    def mark_as_completed(self):
+        self._status = "Completed"
+        print(f"Task {self._task_id} marked as completed.")
+    def get_status(self):
+        return self._status
+    def set_status(self, new_status):
+        allowed_statuses = ["Pending", "In Progress", "Completed"]
+        if new_status in allowed_statuses:
+            self.status = new_status
+            print(f"Status of task {self.title} updated to {new_status}.")
+        else:
+            print(f"Error: Invalid status '{new_status}'. Status not changed.")
 
 task1 = Task(1, "Buy milk", "Buy 2 liters of milk", "Pending")
 task2 = Task(2, "Write plan", "Write a plan for tomorrow", "In Progress")
 task3 = Task(3, "Read article", "Read an article about Python", "Completed")
 
-print(f"Task ID: {task1.task_id}, Title: {task1.title}, Description: {task1.description}, Status: {task1.status}")
+print(f"Task ID: {task1._task_id}, Title: {task1.title}, Description: {task1.description}, Status: {task1._status}")
 
-task2.status = "Completed"
+task2.set_status("Completed")
+print(task2.get_status())
 
-print(f"Task ID: {task2.task_id}, Title: {task2.title}, Description: {task2.description}, Status: {task2.status}")
+print(f"Task ID: {task2._task_id}, Title: {task2.title}, Description: {task2.description}, Status: {task2._status}")
 
 task3.display_info()
+task1.mark_as_completed()
+task1.display_info()
--- to_do/main.py
+++ to_do/main.py
@@ -1,34 +1,56 @@
 
-tasks = []
-
-def addTask():
-    """ Добавляем задачу в список задач """
-    task = input ("Введите задачу: ")
-    tasks.append(task)
-    print (f"Задача '{task}' добавлена в список задач.")
- 
-def listTasks():
-    if not tasks:
-        print("Список задач пуст.")
-    else:
-        print("Список задач:")
-        for index, task in enumerate(tasks):
-            print(f" Задача номер {index+1}. {task}")
-
-def deleteTask():
-    listTasks()
-    try:
-        taskToDelete = int(input("Введите номер задачи, которую хотите удалить: "))
-        index = taskToDelete - 1
-        if index >= 0 and index < len(tasks):
-            tasks.pop(index)
-            print(f"Задача '{taskToDelete}' удалена из списка задач.")
+class Task:
+    def __init__ (self, task_id, title, description, status):
+        self._task_id = task_id
+        self.title = title
+        self.description = description
+        self._status = status
+    def display_info(self):
+        print(f"Task ID: {self._task_id}, Title: {self.title}, Description: {self.description}, Status: {self._status}")
+    def set_status(self, new_status):
+        allowed_statuses = ["Pending", "In Progress", "Completed"]
+        if new_status in allowed_statuses:
+            self._status = new_status
+            print(f"Status of task {self.title} updated to {new_status}.")
         else:
-            print(f"Задача с номером {taskToDelete} не найдена.")
-    except:
-        print("Неверный ввод.")
-    
+            print(f"Error: Invalid status '{new_status}'. Status not changed.")
+    def get_status(self):
+        return self._status
+
+class TaskManager:
+    def __init__ (self):
+        self._tasks_list = []
+        self._next_task_id = 1
+    def add_task(self):
+        task_title = input("Enter task title: ")
+        task_description = input("Enter task description: ")
+        task = Task(self._next_task_id, task_title, task_description, "Pending")
+        self._tasks_list.append(task)
+        self._next_task_id += 1
+        print(f"Task '{task_title}' added with ID {task._task_id}.")
+    def list_task(self):
+        if not self._tasks_list:
+            print("Task list is empty.")
+        else:
+            print("Task list: ")
+            for task in self._tasks_list:
+                task.display_info()
+    def delete_task(self):
+        self.list_task()
+        try:
+            task_to_delete = int(input("Enter the task number you want to delete: "))
+            index = task_to_delete -1
+            if index >= 0 and index < len(self._tasks_list):
+                deleted_task = self._tasks_list.pop(index)
+                print(f"Task '{deleted_task.title}' with ID {deleted_task._task_id} deleted from the task list.")
+            else:
+                print(f"Task with number {task_to_delete} not found.")
+        except ValueError:
+            print("Invalid input. Please enter a valid task number.")
+
 if __name__ == "__main__":
+    my_task = Task()
+    my_task_manager = TaskManager()
     ### Цикл для работы приложения ###
     while True:
         print ("\n")
@@ -42,11 +64,11 @@ if __name__ == "__main__":
         choice = input("Введите номер действия: ")
 
         if (choice == "1"):
-            addTask()
+            my_task_manager.add_task()
         elif (choice == "2"):
-            deleteTask()
+            my_task_manager.delete_task()
         elif (choice == "3"):
-            listTasks()
+            my_task_manager.list_task()
         elif (choice == "4"):
             break
         else:
